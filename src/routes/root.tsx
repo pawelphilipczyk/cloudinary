f14f3def998653d751901794f846595dfebd4086
Paweł Philipczyk <pawel.philipczyk@me.com>
2024-06-07 23:18:39 +0200

    feat: improve logo styling

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,7 @@
-import { cld } from "../api";
 import { scale } from "@cloudinary/url-gen/actions/resize";
+import { cld } from "../api";
 
+import { trim } from "@cloudinary/url-gen/actions/reshape";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "../assets/vite.svg";
 import "./root.css";
@@ -10,17 +11,24 @@ function Root({ children }: React.PropsWithChildren) {
     .image("samples/cloudinary-icon")
     .format("auto")
     .quality("auto")
-    .resize(scale().height(150));
+    .reshape(trim().colorSimilarity(50).colorOverride("transparent"))
+    .resize(scale().height(120));
 
   return (
     <>
       <header>
-        <img src={sample.toURL()} alt="Cloudinary logo" />
+        <a href="https://cloudinary.com/documentation/programmable_media_overview" target="_blank">
+          <img
+            src={sample.toURL()}
+            className="logo cloudinary"
+            alt="Cloudinary logo"
+          />
+        </a>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react spin" alt="React logo" />
         </a>
       </header>
       <main>{children}</main>
