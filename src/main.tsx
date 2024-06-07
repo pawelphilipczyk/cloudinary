961eb2aecc450931f7e874d65967fa944e04e643
Paweł Philipczyk <pawel.philipczyk@me.com>
2024-06-07 11:30:40 +0200

    feat: add gh pages configuration

--- src/main.tsx
+++ src/main.tsx
@@ -7,21 +7,24 @@ import ErrorPage from "./error-page.js";
 import "./index.css";
 import Images, { loader as rootLoader } from "./routes/images.js";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root children={<Outlet />} />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        index: true,
-        loader: rootLoader,
-        element: <Images />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root children={<Outlet />} />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          errorElement: <ErrorPage />,
+          index: true,
+          loader: rootLoader,
+          element: <Images />,
+        },
+      ],
+    },
+  ],
+  { basename: "/clodinary" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
