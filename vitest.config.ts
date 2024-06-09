16f308db3983f866eb0a4113c5247e3d57925999
Paweł Philipczyk <pawel.philipczyk@me.com>
2024-06-09 17:58:08 +0200

    test: exclude playwright tests from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    exclude: [...configDefaults.exclude, '**/tests/**'],
     setupFiles: ['./vitest.setup.ts'],
   },
 });
